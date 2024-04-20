ed3f62cfb4f0dc320611908c513485a731f5a2f8
Salah AlDin <salahqerem@gmail.com>
2024-04-20 20:35:09 +0300

    Add CORS middleware

--- src/modules/index.router.js
+++ src/modules/index.router.js
@@ -1,8 +1,14 @@
 import connectDb from "../../DB/connection.js";
 import noteRouter from "./notes/note.router.js";
+import cors from "cors";
 
 const initApp = async (app, express) => {
+  const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+  };
   connectDb();
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use("/notes", noteRouter);
 };
